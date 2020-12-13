1be918729c8cf6cc76ba6a4144b8ad4a3a83af34
Carlos "Kurolox" Gomez <contact@carlosg.dev>
2020-12-13 13:04:08 +0100

    Day 1 part 1 solved

---
+++ day_1/part_1/main.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+with open(f"{Path(__file__).parents[1]}/input") as problem_input:
+    input_set = set()
+    for line in problem_input:
+        input_set.add(int(line))
+
+for x in input_set:
+    for y in input_set:
+        if x + y == 2020:
+            print(x*y)
+            exit()
